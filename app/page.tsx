0dc08860e0f6023268f38b233ab08dc5b89e3332
yash-reddy01 <yashreddy.official@gmail.com>
2025-08-12 19:13:53 -0500

    initial stage

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <h1><center className={'font-bold flex justify-center h-full items-center text-6xl mt-70'}>Welcome to ArcheTechGlobal!!</center></h1>
+  );
+}
