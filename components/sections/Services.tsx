fae148d7f1b01af2cb4d35e226e212d1d37e1316
yash-reddy01 <yashreddy.official@gmail.com>
2025-08-19 15:40:09 -0500

    trial of component

--- components/sections/Services.tsx
+++ components/sections/Services.tsx
@@ -1,27 +1,64 @@
 "use client"
-
-import React from 'react'
+import { Carousel, Card } from "../ui/apple-cards-carousel"
 
 export const Services = () => {
+  const cards = data.map((card, index) => <Card key={card.src} card={card} index={index} />)
   return (
-    <section id='services' className='w-full py-12 md:py-24 lg:py-32 lg:px-14 bg-muted/50'>
-        <div className='container px-4 md:px-6'>
-            <div className='flex flex-col items-center justify-center space-y-4 text-center'>
-                <div className='space-y-2'>
-                    <div className='inline-block rounded-lg bg-muted px-3 py-1 text-sm'>
-                        Our Services
-                    </div>
-                    <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">What We Offer</h2>
-                    <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                    We provide comprehensive technology solutions to help your business thrive in the digital age.
-                    </p>
-                </div>
-            </div>
-            <div className='max-w-5xl mx-auto px-8'>
-                {/* <Carousel items={cards} /> */}
-            </div>
+    <section id="services" className="w-full py-12 md:py-24 lg:py-32 lg:px-14 bg-muted/50">
+      <div className="container px-4 md:px-6">
+        <div className="flex flex-col items-center justify-center space-y-4 text-center">
+          <div className="space-y-2">
+            <div className="inline-block rounded-lg bg-muted px-3 py-1 text-sm">Our Services</div>
+            <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">What We Offer</h2>
+            <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+              We provide comprehensive technology solutions to help your business thrive in the digital age.
+            </p>
+          </div>
         </div>
+        <div className="max-w-5xl mx-auto px-8">
+          <Carousel items={cards} />
+        </div>
+      </div>
     </section>
   )
 }
 
+const data = [
+  {
+    category: "Artificial Intelligence",
+    title: "You can do more with AI.",
+    src: "https://images.unsplash.com/photo-1593508512255-86ab42a8e620?q=80&w=3556&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    // content: <DummyContent />,
+  },
+  {
+    category: "Productivity",
+    title: "Enhance your productivity.",
+    src: "https://images.unsplash.com/photo-1531554694128-c4c6665f59c2?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    // content: <DummyContent />,
+  },
+  {
+    category: "Product",
+    title: "Launching the new Apple Vision Pro.",
+    src: "https://images.unsplash.com/photo-1713869791518-a770879e60dc?q=80&w=2333&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    // content: <DummyContent />,
+  },
+
+  {
+    category: "Product",
+    title: "Maps for your iPhone 15 Pro Max.",
+    src: "https://images.unsplash.com/photo-1599202860130-f600f4948364?q=80&w=2515&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    // content: <DummyContent />,
+  },
+  {
+    category: "iOS",
+    title: "Photography just got better.",
+    src: "https://images.unsplash.com/photo-1602081957921-9137a5d6eaee?q=80&w=2793&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    // content: <DummyContent />,
+  },
+  {
+    category: "Hiring",
+    title: "Hiring for a Staff Software Engineer",
+    src: "https://images.unsplash.com/photo-1511984804822-e16ba72f5848?q=80&w=2048&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    // content: <DummyContent />,
+  },
+]
